6563b4735193ee78c1b573b8a914bb6a0f4ea072
* <ahmaduabdullahi460@gmail.com>
2022-06-26 18:29:56 +0100

    initial commit

--- I4G012938V9A/blog/models.py
+++ I4G012938V9A/blog/models.py
@@ -6,7 +6,7 @@ class Post(models.Model):
   title = models.CharField(max_length=200)
   text = models.TextField()
   author = models.ForeignKey(User, on_delete=models.CASCADE)
-  created_date = models.DateTimeField
-  published_date = models.DateTimeField
+  created_date = models.DateTimeField(auto_now_add=True)
+  published_date = models.DateTimeField(auto_now_add=True)
   
 
